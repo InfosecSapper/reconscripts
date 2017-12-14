0a0deb9bcbdbea8fb57e2b26195f13ee9ac237c7
TheSapper <piccone.richard@gmail.com>
2017-12-14 18:31:22 +0000

    Working on the initial build.
    Signed-off-by: richard.piccone <piccone.richard@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# reconscripts
+# Reconscripts
+**Reconscripts** is designed to provide a basic level of automation
+for performing initial recon tasks.  
+The individual stages can be requested individually, but the default is to run each script in sequence. An example of a basic workflow is as follows:  
+1. Run the script with a CIDR range; this will run a ping sweep on the range and make note of the hosts which are responsive.
+2. The responsive hosts will then be tested with a more in-depth **nmap** discovery
+3. The subsequent **nmap** results will trigger tertiary scripts based on what services are found (e.g. **dnsenum** if UDP/53 is found open).  
+
+The various outputs will be written to files rather than the console, which will simply provide some progress feedback to the user.
+
+---
+## Disclaimer (of sorts):
+I created this while working through Offensive Security's
+OSCP labs and it is in no way designed to be used commercially,
+professionally, maliciously or for anything other than doing
+grunt work in a lab. It's more of an exercise in Python scripting and using GitHub than it is a tool for recon. It is not designed or intended to replace or augment tools such as **sparta**, **recon-ng**, **nmap**, etc. This script was created after I spent a lot of time using the respective tools individually and manually to understand their operations, features, benefits and flaws; I simply needed a time-saving tool for a scenario in which I had limited use of existing large-scale automation tools which are more mature.  
+To be clear: this was a pet project and I am treating it as disposable. However, I will support the project if there is demand and if I feel I can learn something new, so feel free to highlight issues, suggestions, etc.
--- reconscripts.py
+++ reconscripts.py
@@ -4,6 +4,7 @@ import sys
 import argparse
 import subprocess
 import ipaddress
+import multiprocessing
 
 # ADD COMMAND LINE SWITCHES
 # switches = argparse.ArgumentParser(description='Basic scripted recon tasks.')
@@ -14,8 +15,9 @@ import ipaddress
 # switches.add_argument('-i') # input file          --input-file
 # switches.add_argument('-m') # SNMP ENUMERATION    --snmp              -M  --snmp-off
 # switches.add_argument('-n') # input network       --input-network
-# switches.add_argument('-p') # PINGSWEEP           --ping-sweep        -P  --ping-sweep-off
+# switches.add_argument('-p') # PING SWEEP          --ping-sweep        -P  --ping-sweep-off
 # switches.add_argument('-s') # SSH ENUMERATION     --ssh               -S  --ssh-off
+# switches.add_argument('-t') # SMB ENUMERATION     --smb               -T  --smb-off
 # switches.add_argument('-w') # HTTP ENUMERATION    --http              -W  --http-off
 # switches.add_argument('--prompt') # Prompt to continue or quit (yes/no, default=no)
 
@@ -35,7 +37,54 @@ else:
     neg_ping = []
 
 
-def pingsweep(net):
+def multi_proc(targetin, scanip, port):
+    jobs = []
+    p = multiprocessing.Process(target=targetin, args=(scanip,port))
+    jobs.append(p)
+    p.start()
+    return
+
+
+def sql_enum(ip, port):
+    print("INFO: Detected MS-SQL on %s:%s" % (ip, port))
+    print("INFO: Performing nmap mssql script scan for %s:%s" % (ip, port)
+    MSSQLSCAN = "nmap -vv -sV -Pn -p %s --script=ms-sql-info,ms-sql-config,ms-sql-dump-hashes --script-args=mssql.instance-port=1433,smsql.username-sa,mssql.password-sa -oX results/exam/nmap/%s_mssql.xml %s" % (port, ip, ip)
+    results = subprocess.check_output(MSSQLSCAN, shell=True)
+
+
+def dns_enum(ip, port):
+    print("INFO: Detected DNS on %s:%s" % (ip, port))
+    if port.strip() == "53":
+       SCRIPT = "./dnsrecon.py %s" % ip
+       subprocess.call(SCRIPT, shell=True)
+    return
+
+
+def smtp_enum(ip, port):
+    print("INFO: Detected smtp on %s:%s" % (ip, port))
+    if port.strip() == "25":
+       SCRIPT = "./smtprecon.py %s" % ip
+       subprocess.call(SCRIPT, shell=True)
+    else:
+       print("WARNING: SMTP detected on non-standard port, smtprecon skipped (must run manually)")
+    return
+
+
+def ftp_enum(ip, port):
+    print("INFO: Detected ftp on %s:%s" % (ip, port))
+    SCRIPT = "./ftprecon.py %s %s" % (ip, port)
+    subprocess.call(SCRIPT, shell=True)
+    return
+
+
+def snmp_enum(ip, port):
+    print("INFO: Detected snmp on %s:%s" % (ip, port))
+    SCRIPT = "./snmprecon.py %s" % ip
+    subprocess.call(SCRIPT, shell=True)
+    return
+
+
+def ping_sweep(net):
     """Ping a given network using Linux's native Ping tool."""
     print("Pinging %s addresses..." % net.num_addresses)
     for ip in net:
@@ -48,6 +97,32 @@ def pingsweep(net):
             neg_ping += []
     print("Ping response from %s hosts." % len(pos_ping))
     print("No ping response from %s hosts." % len(neg_ping))
+
+
+def ssh_enum(ip, port):
+    print("INFO: Detected SSH on %s:%s" % (ip, port))
+    SCRIPT = "./sshrecon.py %s %s" % (ip_address, port)
+    subprocess.call(SCRIPT, shell=True)
+    return
+
+
+def smb_enum(ip, port):
+    print("INFO: Detected SMB on %s:%s" % (ip, port))
+    if port.strip() == "445":
+       SCRIPT = "./smbrecon.py %s 2>/dev/null" % ip
+       subprocess.call(SCRIPT, shell=True)
+    return
+
+
+def http_enum(ip, port):
+    print("INFO: Detected http on %s:%s" % (ip, port))
+    print("INFO: Performing nmap web script scan for %s:%s" % (ip, port))
+    HTTPSCAN = "nmap -sV -Pn -vv -p %s --script=http-vhosts,http-userdir-enum,http-apache-negotiation,http-backup-finder,http-config-backup,http-default-accounts,http-email-harvest,http-methods,http-method-tamper,http-passwd,http-robots.txt -oN /root/scripts/recon_enum/results/exam/%s_http.nmap %s" % (port, ip_address, ip_address)
+    results = subprocess.check_output(HTTPSCAN, shell=True)
+    DIRBUST = "./dirbust.py http://%s:%s %s" % (ip_address, port, ip_address)  # execute the python script
+    subprocess.call(DIRBUST, shell=True)
+    # NIKTOSCAN = "nikto -host %s -p %s > %s._nikto" % (ip_address, port, ip_address)
+    return
 
 
 if __name__ == '__main__':
